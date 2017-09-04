4df7cec9a2c363ff816d3935a82fb6ad766d30e2
gg <gueltro@gmail.com>
2017-09-04 17:24:36 -0400

    First iteration of tree_map_reduce and tree_reduce

--- src/TreeReducer.jl
+++ src/TreeReducer.jl
@@ -1,5 +1,21 @@
 module TreeReducer
+    using DistributedArrays
 
-# package code goes here
+    export tree_reduce, tree_map_reduce
+
+    function tree_reduce(op::Function, darray::DArray)
+        return tree_map_reduce((x -> x), op, darray)
+    end
+
+    function tree_map_reduce(f::Function, op::Function, darray::DArray)
+        workers = [p for p in procs(darray)]
+        futures = [@spawnat w local_map_reduce(f, op, darray) for w in workers]
+
+        return mapreduce(fetch, op, futures)
+    end
+
+    function local_map_reduce(f::Function, op::Function, darray::DArray)
+        return mapreduce(f, op, localpart(darray))
+    end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using TreeReducer
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using DistributedArrays
+
+# TreeMapReduce
+f = (x -> 2 * x)
+op = +
+array = [i for i=1:10^3]
+darray = distribute(array)
+
+@test reduce(op, array) == tree_reduce(op, darray)
+@test mapreduce(f, op, array) == tree_map_reduce(f, op, darray)
