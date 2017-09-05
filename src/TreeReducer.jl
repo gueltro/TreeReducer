67e31c483d4ab73fb722b7f0d9ad28ee25f614a5
gg <gueltro@gmail.com>
2017-09-05 02:58:34 -0400

    Generalize TreeReducer, remove DistributedArrays from dependencies

--- src/TreeReducer.jl
+++ src/TreeReducer.jl
@@ -1,21 +1,9 @@
 module TreeReducer
-    using DistributedArrays
+    export tree_map_reduce
 
-    export tree_reduce, tree_map_reduce
-
-    function tree_reduce(op::Function, darray::DArray)
-        return tree_map_reduce((x -> x), op, darray)
-    end
-
-    function tree_map_reduce(f::Function, op::Function, darray::DArray)
-        workers = [p for p in procs(darray)]
-        futures = [@spawnat w local_map_reduce(f, op, darray) for w in workers]
+    function tree_map_reduce(f::Function, op::Function, workers::Array{Int}=procs())
+        futures = [@spawnat w f() for w in workers]
 
         return mapreduce(fetch, op, futures)
     end
-
-    function local_map_reduce(f::Function, op::Function, darray::DArray)
-        return mapreduce(f, op, localpart(darray))
-    end
-
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,8 @@ op = +
 array = [i for i=1:10^3]
 darray = distribute(array)
 
-@test reduce(op, array) == tree_reduce(op, darray)
-@test mapreduce(f, op, array) == tree_map_reduce(f, op, darray)
+f_clojure = (() -> mapreduce(f, op, localpart(darray)))
+@test mapreduce(f, op, array) == tree_map_reduce(f_clojure, op)
+
+
+## TreeMapReduce on distributed arrays
